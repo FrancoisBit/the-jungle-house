86afa46569634592859baae8d7f3e572cc9fdd49
FRANCOIS BIT <bit.francois@gmail.com>
2022-07-24 13:58:21 +0300

    Update shoppingList

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import "../css/Banner.css";
+import logo from "../assets/logo.png"
+
+function Banner() {
+	const title = "The jungle house";
+    return (
+        <div className="banner">
+			<img src={logo} alt={title} className="logo" />
+            <h1>{title}</h1>
+        </div>
+    );
+}
+
+export default Banner;
